bf01f262f6ffcf7496001831809659a55cdd516f
meljanat <mdjanati.mj@gmail.com>
2025-07-25 16:54:38 +0100

    valuesOfATuple

---
+++ tuples_refs/src/lib.rs
@@ -0,0 +1,17 @@
+pub struct Student {
+    pub id: u32,
+    pub first_name: String,
+    pub last_name: String,
+}
+
+pub fn id(student: &Student) -> u32 {
+    student.id
+}
+
+pub fn first_name(student: &Student) -> &str {
+    &student.first_name
+}
+
+pub fn last_name(student: &Student) -> &str {
+    &student.last_name
+}
