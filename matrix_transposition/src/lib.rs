e62bf63e6adc5a28b42c1c8a3dae4bccc2add582
meljanat <mdjanati.mj@gmail.com>
2025-07-25 16:33:00 +0100

    add pub to the matrix declaration

--- matrix_transposition/src/lib.rs
+++ matrix_transposition/src/lib.rs
@@ -1,5 +1,11 @@
-pub struct Matrix ((i32, i32), (i32, i32));
+pub struct Matrix(
+    pub (i32, i32),
+    pub (i32, i32)
+);
 
 pub fn transpose(m: Matrix) -> Matrix {
-    Matrix ((m.0.1, m.1.0), (m.0.0, m.1.1))
+    Matrix(
+        (m.0.0, m.1.0),
+        (m.0.1, m.1.1)
+    )
 }
