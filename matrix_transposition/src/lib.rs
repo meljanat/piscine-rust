b0edbb1a2d863fdd5dd1bfb45a1b3670296b5617
meljanat <mdjanati.mj@gmail.com>
2025-07-25 16:38:16 +0100

    add derive

--- matrix_transposition/src/lib.rs
+++ matrix_transposition/src/lib.rs
@@ -1,11 +1,9 @@
-pub struct Matrix(
-    pub (i32, i32),
-    pub (i32, i32)
-);
+#[derive(Debug, PartialEq, Eq)]
+pub struct Matrix(pub (i32, i32), pub (i32, i32));
 
 pub fn transpose(m: Matrix) -> Matrix {
     Matrix(
-        (m.0.0, m.1.0),
-        (m.0.1, m.1.1)
+        (m.0 .0, m.1 .0),
+        (m.0 .1, m.1 .1),
     )
 }
