568fdb3ea1744a54acf3011b44b33ae50421b1d2
meljanat <mdjanati.mj@gmail.com>
2025-07-25 16:59:03 +0100

    fixSyntax

--- tuples_refs/src/lib.rs
+++ tuples_refs/src/lib.rs
@@ -1,17 +1,17 @@
 pub struct Student (
-    pub id: u32,
-    pub first_name: String,
-    pub last_name: String,
+    pub u32,
+    pub String,
+    pub String,
 );
 
 pub fn id(student: &Student) -> u32 {
-    student.id
+    student.0
 }
 
 pub fn first_name(student: &Student) -> &str {
-    &student.first_name
+    &student.1
 }
 
 pub fn last_name(student: &Student) -> &str {
-    &student.last_name
+    &student.2
 }
