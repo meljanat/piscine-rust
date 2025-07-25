ec3b7ba6f11114183253ce8bd8fe5a20df2ab202
meljanat <mdjanati.mj@gmail.com>
2025-07-25 16:23:35 +0100

    matrixTranspose

--- matrix_transposition/src/lib.rs
+++ matrix_transposition/src/lib.rs
@@ -1,11 +1,5 @@
-struct Matrix {
-    tuple1: (i32, i32),
-    tuple2: (i32, i32),
-}
+struct Matrix ((i32, i32), (i32, i32));
 
 pub fn transpose(m: Matrix) -> Matrix {
-    Matrix {
-        tuple1: (m.tuple1.0, m.tuple2.0),
-        tuple2: (m.tuple1.1, m.tuple2.1),
-    }
+    Matrix ((m.0.1, m.1.0), (m.0.0, m.1.1))
 }
