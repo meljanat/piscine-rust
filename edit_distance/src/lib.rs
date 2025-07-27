3cf8a7423a57e0b974e67a21f840aca558609a4b
meljanat <mdjanati.mj@gmail.com>
2025-07-27 15:12:23 +0100

    .

--- edit_distance/src/lib.rs
+++ edit_distance/src/lib.rs
@@ -2,11 +2,18 @@ pub fn edit_distance(source: &str, target: &str) -> usize {
     if source == target {
         return 0;
     }
+
+    let source_chars: Vec<char> = source.chars().collect();
+    let target_chars: Vec<char> = target.chars().collect();
+
+    let min_len = source_chars.len().min(target_chars.len());
     let mut changes = 0;
-    for i in 0..target.len() {
-        if target[i] != source[i] {
+
+    for i in 0..min_len {
+        if source_chars[i] != target_chars[i] {
             changes += 1;
         }
     }
-    changes
+
+    changes + (source_chars.len().max(target_chars.len()) - min_len)
 }
