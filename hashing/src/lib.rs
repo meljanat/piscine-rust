7069478c9c5e0c50c8c1e54c9821e596e5c33023
meljanat <mdjanati.mj@gmail.com>
2025-07-27 17:29:39 +0100

    small fix

--- hashing/src/lib.rs
+++ hashing/src/lib.rs
@@ -8,6 +8,10 @@ pub fn mean(list: &[i32]) -> f64 {
 pub fn median(list: &[i32]) -> i32 {
     let mut sorted = list.to_vec();
     sorted.sort();
+    if sorted.len() % 2 == 0 {
+        let mid = sorted.len() / 2;
+        return (sorted[mid - 1] + sorted[mid]) / 2;
+    }
     sorted[sorted.len() / 2]
 }
 
