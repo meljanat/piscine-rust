d172e1eea211f996b43996f4a9da95e431d3037b
meljanat <mdjanati.mj@gmail.com>
2025-07-27 14:33:47 +0100

    capitalizing and strings manipulation

---
+++ capitalizing/src/lib.rs
@@ -0,0 +1,21 @@
+pub fn capitalize_first(input: &str) -> String {
+    input[..1].to_uppercase() + &input[1..]
+}
+
+pub fn title_case(input: &str) -> String {
+    input.split_whitespace()
+        .map(|word| capitalize_first(word))
+        .collect::<Vec<String>>()
+        .join(" ")
+}
+
+pub fn change_case(input: &str) -> String {
+    input.chars()
+        .map(|c| 
+        if c.is_uppercase() {
+            c.to_lowercase().to_string()
+        } else {
+            c.to_uppercase().to_string()
+        })
+        .collect::<String>()
+}
