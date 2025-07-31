0f427d8c9bd1605ed2e40d126d9817bcd9ef10b4
meljanat <mdjanati.mj@gmail.com>
2025-07-31 15:43:51 +0100

    unwrap_or_expect

---
+++ unwrap_or_expect/src/lib.rs
@@ -0,0 +1,37 @@
+use core::panic;
+
+pub enum Security {
+    Unknown,
+    Message,
+    Warning,
+    NotFound,
+    UnexpectedUrl,
+}
+
+pub fn fetch_data(server: Result<&str, &str>, security_level: Security) -> String {
+    match server {
+        Ok(data) => {
+            if security_level == Security::UnexpectedUrl {
+                panic!("{data}");
+            }
+            data.to_string()
+        }
+        Err(err) => match security_level {
+            Security::Unknown => {
+                panic!();
+            }
+            Security::Message => {
+                panic!("ERROR: program stops");
+            }
+            Security::Warning => {
+               "WARNING: check the server".to_string()
+            }
+            Security::NotFound => {
+                "Not found: {err}".to_string()
+            }
+            Security::UnexpectedUrl => {
+                err.to_string()
+            }
+        },
+    }
+}
