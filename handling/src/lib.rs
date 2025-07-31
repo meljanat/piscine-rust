3213ed0fc0f16a6335c1d0d911504c2ea48268a4
meljanat <mdjanati.mj@gmail.com>
2025-07-31 16:50:34 +0100

    create or append to file

---
+++ handling/src/lib.rs
@@ -0,0 +1,9 @@
+use std::fs::OpenOptions;
+
+pub fn open_or_create<P: AsRef<Path>>(path: &P, content: &str) {
+    let file = OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open(path)
+    file.write_all(content.as_bytes());
+}
