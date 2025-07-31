c27157ca7a025c01f046da62f4561b150b203c70
meljanat <mdjanati.mj@gmail.com>
2025-07-31 16:53:20 +0100

    up

--- handling/src/lib.rs
+++ handling/src/lib.rs
@@ -1,9 +1,8 @@
 use std::fs::OpenOptions;
+use std::io::Write;
+use std::path::Path;
 
 pub fn open_or_create<P: AsRef<Path>>(path: &P, content: &str) {
-    let file = OpenOptions::new()
-        .append(true)
-        .create(true)
-        .open(path)
+    let file = OpenOptions::new().append(true).create(true).open(path);
     file.write_all(content.as_bytes());
 }
