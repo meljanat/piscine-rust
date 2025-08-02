b0848e83c45de687b42945c50a34769135ac8022
meljanat <mdjanati.mj@gmail.com>
2025-08-02 12:15:32 +0100

    update

--- middle_day/src/lib.rs
+++ middle_day/src/lib.rs
@@ -1,4 +1,4 @@
-use chrono::{NaiveDate, Weekday};
+use chrono::{Datelike, NaiveDate};
 
 pub fn middle_day(year: u32) -> Option<chrono::Weekday> {
     if (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0) {
