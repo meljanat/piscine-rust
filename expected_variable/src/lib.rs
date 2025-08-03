a175b9a6ffc297c64d23c933dfd13e5254dc2a41
Mouad El Janati <mdjanati.mj@gmail.com>
2025-08-03 06:47:32 +0100

    expected variable

---
+++ expected_variable/src/lib.rs
@@ -0,0 +1,23 @@
+use case::{is_camel_case, is_snake_case, CaseExt};
+use edit_distance::edit_distance;
+
+pub fn expected_variable(compared: &str, expected: &str) -> Option<String> {
+    if !is_camel_case(compared) && !is_snake_case(compared) {
+        return None;
+    }
+
+    let a = compared.to_lowercase();
+    let b = expected.to_lowercase();
+
+    let dist = edit_distance(&a, &b);
+    let max_len = a.len().max(b.len());
+
+    let similarity = 1.0 - dist as f64 / max_len as f64;
+
+    if similarity > 0.5 {
+        let percent = (similarity * 100.0).round() as usize;
+        Some(format!("{}%", percent))
+    } else {
+        None
+    }
+}
