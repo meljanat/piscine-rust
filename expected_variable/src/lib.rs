3768054b0033331d36890d9bed212ed77e3f55bb
Mouad El Janati <mdjanati.mj@gmail.com>
2025-08-03 07:06:47 +0100

    test

--- expected_variable/src/lib.rs
+++ expected_variable/src/lib.rs
@@ -2,6 +2,10 @@ use convert_case::{Case, Casing};
 use edit_distance::edit_distance;
 
 pub fn expected_variable(compared: &str, expected: &str) -> Option<String> {
+    if !compared.is_case(Case::Camel) && !compared.is_case(Case::Snake) {
+        return None;
+    }
+
     let a = compared.to_lowercase();
     let b = expected.to_lowercase();
 
