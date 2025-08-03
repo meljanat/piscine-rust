9c06f481a4a0fdabebdf4a36040fe47e55e0679f
Mouad El Janati <mdjanati.mj@gmail.com>
2025-08-03 04:42:41 +0100

    talking fix...

--- talking/src/lib.rs
+++ talking/src/lib.rs
@@ -1,12 +1,12 @@
 pub fn talking(text: &str) -> &str {
-    text = text.trim();
-    if text.chars().all(|c| (c.is_alphabetic() && c.is_ascii_uppercase()) || c == '!') && text.ends_with('!') {
+    let trimmed = text.trim();
+    if trimmed.chars().all(|c| (c.is_alphabetic() && c.is_ascii_uppercase()) || c == '!') && trimmed.ends_with('!') {
         "There is no need to yell, calm down!"
-    } else if text.chars().all(|c| (c.is_alphabetic() && c.is_ascii_uppercase()) || c == '?') && text.ends_with('?') {
+    } else if trimmed.chars().all(|c| (c.is_alphabetic() && c.is_ascii_uppercase()) || c == '?') && trimmed.ends_with('?') {
         "Quiet, I am thinking!"
-    } else if text.chars().any(|c| c.is_ascii_lowercase()) && text.ends_with('?') {
+    } else if trimmed.chars().any(|c| c.is_ascii_lowercase()) && trimmed.ends_with('?') {
         "Sure."
-    } else if text.is_empty() {
+    } else if trimmed.is_empty() {
         "Just say something!"
     } else {
         "Interesting"
