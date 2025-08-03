98b5bf656760b30f88f5cbb81cafce295febf695
Mouad El Janati <mdjanati.mj@gmail.com>
2025-08-03 03:51:41 +0100

    better method

--- searching/src/lib.rs
+++ searching/src/lib.rs
@@ -1,5 +1,3 @@
 pub fn search(array: &[i32], key: i32) -> Option<usize> {
-    let mut reversed = array.to_vec();
-    reversed.reverse();
-    reversed.iter().position(|&x| x == key)
+    array.iter().rposition(|&x| x == key)
 }
