501cfac52d7b8cd49720d6351dc16dbccbaf6a67
Mouad El Janati <mdjanati.mj@gmail.com>
2025-08-03 04:41:44 +0100

    talking fix

--- talking/src/lib.rs
+++ talking/src/lib.rs
@@ -1,9 +1,10 @@
 pub fn talking(text: &str) -> &str {
-    if text.chars().all(|c| c.is_ascii_uppercase() || c == '!') {
+    text = text.trim();
+    if text.chars().all(|c| (c.is_alphabetic() && c.is_ascii_uppercase()) || c == '!') && text.ends_with('!') {
         "There is no need to yell, calm down!"
-    } else if text.chars().all(|c| c.is_ascii_uppercase() || c == '?') {
+    } else if text.chars().all(|c| (c.is_alphabetic() && c.is_ascii_uppercase()) || c == '?') && text.ends_with('?') {
         "Quiet, I am thinking!"
-    } else if text.chars().any(|c| c.is_ascii_lowercase() || c == '?') {
+    } else if text.chars().any(|c| c.is_ascii_lowercase()) && text.ends_with('?') {
         "Sure."
     } else if text.is_empty() {
         "Just say something!"
