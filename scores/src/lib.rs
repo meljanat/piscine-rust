b5826dc261c9c123bf1b64211b8e6793e5a68e03
Mouad El Janati <mdjanati.mj@gmail.com>
2025-08-03 03:39:27 +0100

    scores

---
+++ scores/src/lib.rs
@@ -0,0 +1,14 @@
+pub fn score(input: &str) -> u64 {
+    input.chars().fold(0, |acc, c| {
+        acc + match c.to_ascii_uppercase() {
+            'A' | 'E' | 'I' | 'O' | 'U' | 'L' | 'N' | 'R' | 'S' | 'T' => 1,
+            'D' | 'G' => 2,
+            'B' | 'C' | 'M' | 'P' => 3,
+            'F' | 'H' | 'V' | 'W' | 'Y' => 4,
+            'K' => 5,
+            'J' | 'X' => 8,
+            'Q' | 'Z' => 10,
+            _ => 0,
+        }
+    })
+}
