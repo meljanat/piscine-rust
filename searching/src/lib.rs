4d8da52a1996db43a19e7090b86426e84f757008
Mouad El Janati <mdjanati.mj@gmail.com>
2025-08-03 03:50:01 +0100

    fix

--- searching/src/lib.rs
+++ searching/src/lib.rs
@@ -1,3 +1,5 @@
 pub fn search(array: &[i32], key: i32) -> Option<usize> {
-    array.to_vec().reverse().iter().position(|&x| x == key)
+    let mut reversed = array.to_vec();
+    reversed.reverse();
+    reversed.iter().position(|&x| x == key)
 }
