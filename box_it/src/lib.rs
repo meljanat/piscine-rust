12d453a57c24bad01e14e34fe91b1c4df68ee928
meljanat <mdjanati.mj@gmail.com>
2025-08-07 14:46:40 +0100

    up

--- box_it/src/lib.rs
+++ box_it/src/lib.rs
@@ -1,6 +1,15 @@
 pub fn parse_into_boxed(s: String) -> Vec<Box<u32>> {
     s.split(' ')
-        .filter_map(|word| word.parse::<u32>().ok())
+        .filter_map(|word| {
+            if word.ends_with('k') {
+                word.trim_end_matches('k')
+                    .parse::<f64>()
+                    .ok()
+                    .map(|num| (num * 1000.0) as u32)
+            } else {
+                word.parse::<u32>().ok()
+            }
+        })
         .map(|num| Box::new(num))
         .collect()
 }
