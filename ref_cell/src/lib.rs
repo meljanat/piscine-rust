593bf80e0e0955eea4b967197dd5bc823c9eb0bf
meljanat <mdjanati.mj@gmail.com>
2025-08-09 15:57:22 +0100

    ref cell

---
+++ ref_cell/src/lib.rs
@@ -0,0 +1,36 @@
+pub struct Tracker {
+    pub messages: Vec<String>,
+    pub value: i32,
+    pub max: i32,
+}
+
+impl Tracker {
+    pub fn new(max: i32) -> Self {
+        Self {
+            messages: Vec::new(),
+            value: 0,
+            max,
+        }
+    }
+
+    pub fn set_value(&mut self, value: i32) {
+        if value > self.max {
+            self.messages.push("Error: You can't go over your quota!");
+        } else if value > self.max as f32 * 0.7 {
+            self.messages.push(format!(
+                "Warning: You have used up over {}% of your quota!",
+                (value as f32 / self.max as f32 * 100.0).round()
+            ));
+        } else {
+            self.value = value;
+        }
+    }
+
+    pub fn peek(&mut self, &value: Rc<i32>) {
+        let count = Rc::strong_count(&value);
+        self.messages.push(format!(
+            "Info: This value would use {}% of your quota",
+            (count as f32 / self.max as f32 * 100.0).round()
+        ));
+    }
+}
