47d20d21c2a1d05bdbae90198299253b8fef8a62
meljanat <mdjanati.mj@gmail.com>
2025-08-09 21:16:52 +0100

    linked list

---
+++ generics_list/src/lib.rs
@@ -0,0 +1,41 @@
+#[derive(Clone, Debug)]
+pub struct List<T> {
+    pub head: Option<Node<T>>,
+}
+
+#[derive(Clone, Debug)]
+pub struct Node<T> {
+    pub value: T,
+    pub next: Option<Box<Node<T>>>,
+}
+
+impl<T> List<T> {
+    pub fn new() -> List<T> {
+        List { head: None }
+    }
+
+    pub fn push(&mut self, value: T) {
+        let new_node = Node {
+            value,
+            next: self.head.take().map(Box::new),
+        };
+        self.head = Some(new_node);
+    }
+
+    pub fn pop(&mut self) {
+        self.head = match self.head.take() {
+            Some(node) => node.next.map(|e| *e),
+            None => None,
+        };
+    }
+
+    pub fn len(&self) -> usize {
+        let mut count = 0;
+        let mut current = self.head.as_ref();
+        while let Some(node) = current {
+            count += 1;
+            current = node.next.as_deref();
+        }
+        count
+    }
+}
