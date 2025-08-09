7941b377d37dbf318dd90cd8c3295e2c0b2a7c0e
meljanat <mdjanati.mj@gmail.com>
2025-08-09 22:20:48 +0100

    small fix

--- easy_traits/src/lib.rs
+++ easy_traits/src/lib.rs
@@ -14,16 +14,16 @@ pub trait AppendStr {
 impl AppendStr for StringValue {
     fn append_str(&mut self, str_to_append: String) -> Self {
         self.value.push_str(&str_to_append);
-        self
+        self.clone()
     }
 
     fn append_number(&mut self, nb_to_append: f64) -> Self {
         self.value.push_str(&nb_to_append.to_string());
-        self
+        self.clone()
     }
 
     fn remove_punctuation_marks(&mut self) -> Self {
         self.value.retain(|c| !c.is_ascii_punctuation());
-        self
+        self.clone()
     }
 }
