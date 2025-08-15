5b8cddfc2f8e42666d6a499d3b56eeba2c88b988
meljanat <mdjanat.mj@gmail.com>
2025-08-15 15:36:21 +0100

    border_cross

---
+++ border_cross/src/lib.rs
@@ -0,0 +1,45 @@
+#[allow(dead_code)]
+pub struct Car<'a> {
+    pub plate_nbr: &'a str,
+    pub model: &'a str,
+    pub horse_power: u32,
+    pub year: u32,
+}
+
+#[allow(dead_code)]
+pub struct Truck<'a> {
+    pub plate_nbr: &'a str,
+    pub model: &'a str,
+    pub horse_power: u32,
+    pub year: u32,
+    pub load_tons: u32,
+}
+
+pub trait Vehicle<'a> {
+    fn model(&self) -> &'a str;
+    fn year(&self) -> u32;
+}
+
+impl<'a> Vehicle<'a> for Truck<'a> {
+    fn model(&self) -> &'a str {
+        self.model
+    }
+
+    fn year(&self) -> u32 {
+        self.year
+    }
+}
+
+impl<'a> Vehicle<'a> for Car<'a> {
+    fn model(&self) -> &'a str {
+        self.model
+    }
+
+    fn year(&self) -> u32 {
+        self.year
+    }
+}
+
+fn all_models(list: Vec<&Vehicle>) -> Vec<&str> {
+    list.iter().map(|v| v.model()).collect()
+}
