b8ab65ffcf051a2a001822699bd77aae00025c22
meljanat <mdjanat.mj@gmail.com>
2025-08-15 15:45:47 +0100

    border_cross

--- border_cross/src/lib.rs
+++ border_cross/src/lib.rs
@@ -40,6 +40,6 @@ impl<'a> Vehicle<'a> for Car<'a> {
     }
 }
 
-fn all_models<'a>(list: Vec<&'a dyn Vehicle<'a>>) -> Vec<&'a str> {
+pub fn all_models<'a>(list: Vec<&'a dyn Vehicle<'a>>) -> Vec<&'a str> {
     list.iter().map(|v| v.model()).collect()
 }
