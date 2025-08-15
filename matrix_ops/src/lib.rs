d2cff6df85e569ba644bc8bf3c36348ff4987859
meljanat <mdjanat.mj@gmail.com>
2025-08-15 14:25:39 +0100

    add/sub two matrices

--- matrix_ops/src/lib.rs
+++ matrix_ops/src/lib.rs
@@ -2,38 +2,37 @@ use lalgebra_scalar::Scalar;
 use matrix::Matrix;
 use std::ops::{Add, Sub};
 
+#[derive(Debug, Clone)]
 pub struct MatrixOps<T>(pub Matrix<T>);
 
 impl<T: Scalar<Item = T>> Add for MatrixOps<T> {
     type Output = Option<Self>;
-    fn add(&self, other: &Self) -> Option<Self> {
-        if self.0.len() != other.0.len() || self.0[0].len() != other.0[0].len() {
+    fn add(self, other: Self) -> Self::Output {
+        if self.0.0.len() != other.0.0.len() || self.0.0[0].len() != other.0.0[0].len() {
             return None;
         }
-
-        let mut result = Matrix::zero(self.0.len(), self.0[0].len());
-        for i in 0..self.0.len() {
-            for j in 0..self.0[0].len() {
-                result.0[i][j] = self.0[i][j] + other.0[i][j];
+        let mut result = Matrix::zero(self.0.0.len(), self.0.0[0].len());
+        for i in 0..self.0.0.len() {
+            for j in 0..self.0.0[0].len() {
+                result.0[i][j] = self.0.0[i][j] + other.0.0[i][j];
             }
         }
-        Some(result)
+        Some(MatrixOps(result))
     }
 }
 
 impl<T: Scalar<Item = T>> Sub for MatrixOps<T> {
     type Output = Option<Self>;
-    fn sub(&self, other: &Self) -> Option<Self> {
-        if self.0.len() != other.0.len() || self.0[0].len() != other.0[0].len() {
+    fn sub(self, other: Self) -> Self::Output {
+        if self.0.0.len() != other.0.0.len() || self.0.0[0].len() != other.0.0[0].len() {
             return None;
         }
-
-        let mut result = Matrix::zero(self.0.len(), self.0[0].len());
-        for i in 0..self.0.len() {
-            for j in 0..self.0[0].len() {
-                result.0[i][j] = self.0[i][j] - other.0[i][j];
+        let mut result = Matrix::zero(self.0.0.len(), self.0.0[0].len());
+        for i in 0..self.0.0.len() {
+            for j in 0..self.0.0[0].len() {
+                result.0[i][j] = self.0.0[i][j] - other.0.0[i][j];
             }
         }
-        Some(result)
+        Some(MatrixOps(result))
     }
 }
