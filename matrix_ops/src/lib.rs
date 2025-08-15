e228d344bcbe8713f277a136a4d3fda0aa0401d0
meljanat <mdjanat.mj@gmail.com>
2025-08-15 14:43:50 +0100

    add/sub two matrices

--- matrix_ops/src/lib.rs
+++ matrix_ops/src/lib.rs
@@ -1,38 +1,55 @@
 use lalgebra_scalar::Scalar;
-pub use matrix::Matrix;
 use std::ops::{Add, Sub};
 
-#[derive(Debug, Clone)]
-pub struct MatrixOps<T>(pub Matrix<T>);
+#[derive(Debug, Clone, PartialEq)]
+pub struct Matrix<T>(pub Vec<Vec<T>>);
 
-impl<T: Scalar<Item = T>> Add for MatrixOps<T> {
-    type Output = Option<Self>;
-    fn add(self, other: Self) -> Self::Output {
-        if self.0.0.len() != other.0.0.len() || self.0.0[0].len() != other.0.0[0].len() {
-            return None;
+impl<T: Scalar<Item = T>> Matrix<T> {
+    pub fn new() -> Matrix<T> {
+        Matrix(Vec::new())
+    }
+
+    pub fn zero(row: usize, col: usize) -> Matrix<T> {
+        Matrix(vec![vec![T::zero(); col]; row])
+    }
+
+    pub fn identity(n: usize) -> Matrix<T> {
+        let mut mat = Matrix::zero(n, n);
+        for i in 0..n {
+            mat.0[i][i] = T::one();
         }
-        let mut result = Matrix::zero(self.0.0.len(), self.0.0[0].len());
-        for i in 0..self.0.0.len() {
-            for j in 0..self.0.0[0].len() {
-                result.0[i][j] = self.0.0[i][j] + other.0.0[i][j];
-            }
-        }
-        Some(MatrixOps(result))
+        mat
     }
 }
 
-impl<T: Scalar<Item = T>> Sub for MatrixOps<T> {
+impl<T: Scalar<Item = T>> Add for Matrix<T> {
     type Output = Option<Self>;
-    fn sub(self, other: Self) -> Self::Output {
-        if self.0.0.len() != other.0.0.len() || self.0.0[0].len() != other.0.0[0].len() {
+    fn add(self, other: Self) -> Self::Output {
+        if self.0.len() != other.0.len() || self.0[0].len() != other.0[0].len() {
             return None;
         }
-        let mut result = Matrix::zero(self.0.0.len(), self.0.0[0].len());
-        for i in 0..self.0.0.len() {
-            for j in 0..self.0.0[0].len() {
-                result.0[i][j] = self.0.0[i][j] - other.0.0[i][j];
+        let mut result = Matrix::zero(self.0.len(), self.0[0].len());
+        for i in 0..self.0.len() {
+            for j in 0..self.0[0].len() {
+                result.0[i][j] = self.0[i][j] + other.0[i][j];
             }
         }
-        Some(MatrixOps(result))
+        Some(result)
+    }
+}
+
+impl<T: Scalar<Item = T>> Sub for Matrix<T> {
+    type Output = Option<Self>;
+    fn sub(self, other: Self) -> Self::Output {
+        if self.0.len() != other.0.len() || self.0[0].len() != other.0[0].len() {
+            return None;
+        }
+        let mut result = Matrix::zero(self.0.len(), self.0[0].len());
+        for i in 0..self.0.len() {
+            for j in 0..self.0[0].len() {
+                result.0[i][j] = self.0[i][j] - other.0[i][j];
+            }
+        }
+        Some(result)
     }
 }
